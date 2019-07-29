f7e4005e71bb62e4475ccd3b7e17d3d6ee6f8861
ardi <ardi.krasniqi99@gmail.com>
2019-07-29 21:15:57 +0200

    crud user

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 const mongoose = require('mongoose')
+const morgan = require('morgan')
+
 const DB_URI = 'mongodb+srv://issue-tracker:issue-tracker@issue-tracker-75nez.mongodb.net/test?retryWrites=true&w=majority';
 
 //Connecting to our mongodb database
@@ -10,6 +12,64 @@ mongoose.connect(DB_URI)
     .then(() => {
         console.log("Connected to DB")
     })
+
+
+app.use(morgan(''))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const UserModel = require('./models/user.model');
+//Create user
+app.post('/user', (req, res, next) => {
+    let user = new UserModel({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password,
+    })
+    console.log(req.body);
+    UserModel.create(user, (err, response) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        res.json(response);
+    })
+})
+
+app.get('/user', (req, res) => {
+    UserModel.find({}, (err, result) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        res.json(result);
+    })
+})
+
+app.get('/userById/:id', (req, res) => {
+    const id = req.params.id;
+    UserModel.findById({ _id: id }, (err, result) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        res.json(result);
+    })
+})
+
+//Delete  user
+app.delete('/user/:id', (req, res, next) => {
+    const id = req.params.id;
+    UserModel.deleteOne({ _id: id }, (err, result) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        res.json(result);
+    })
+})
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
